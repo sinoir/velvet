3f3dac4a6598b239004f904d42f24286fba169ae
Adam Bednarek <adam@ad60.com>
2014-05-27 13:56:25 -0400

    Extend PhotoHash
    - Include getting child resolutions easily

--- Delectable/app/src/main/java/com/delectable/mobile/api/models/PhotoHash.java
+++ Delectable/app/src/main/java/com/delectable/mobile/api/models/PhotoHash.java
@@ -1,13 +1,12 @@
 package com.delectable.mobile.api.models;
 
-import java.util.Map;
+import com.google.gson.annotations.SerializedName;
 
 public class PhotoHash {
 
     String url;
 
-    // TODO: Build custom object for each resolution or hash of Ids?
-    Map<String, String> child_resolutions;
+    ChildResolution child_resolutions;
 
     public String getUrl() {
         return url;
@@ -17,11 +16,68 @@ public class PhotoHash {
         this.url = url;
     }
 
-    public Map<String, String> getChildResolutions() {
+    public ChildResolution getChildResolutions() {
         return child_resolutions;
     }
 
-    public void setChildResolutions(Map<String, String> child_resolutions) {
+    public void setChildResolutions(ChildResolution child_resolutions) {
         this.child_resolutions = child_resolutions;
+    }
+
+    public String getNanoUrl() {
+        return this.child_resolutions.size_nano;
+    }
+
+    public String getMicroUrl() {
+        return this.child_resolutions.size_micro;
+    }
+
+    public String getThumbUrl() {
+        return this.child_resolutions.size_thumb;
+    }
+
+    public String get250Url() {
+        return this.child_resolutions.size_250;
+    }
+
+    public String getMediumUrl() {
+        return this.child_resolutions.size_medium;
+    }
+
+    class ChildResolution {
+
+        @SerializedName("50")
+        String size_nano;
+
+        @SerializedName("110")
+        String size_micro;
+
+        @SerializedName("150")
+        String size_thumb;
+
+        @SerializedName("250")
+        String size_250;
+
+        @SerializedName("640")
+        String size_medium;
+
+        @Override
+        public String toString() {
+            return "ChildResolution{" +
+                    "size_nano='" + size_nano + '\'' +
+                    ", size_micro='" + size_micro + '\'' +
+                    ", size_thumb='" + size_thumb + '\'' +
+                    ", size_250='" + size_250 + '\'' +
+                    ", size_medium='" + size_medium + '\'' +
+                    '}';
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "PhotoHash{" +
+                "url='" + url + '\'' +
+                ", child_resolutions=" + child_resolutions +
+                '}';
     }
 }
