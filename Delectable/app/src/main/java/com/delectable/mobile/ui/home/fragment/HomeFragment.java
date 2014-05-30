52849c94f6ae21a1bed3921561eb52e89bf8abb5
Adam Bednarek <adam@ad60.com>
2014-05-30 12:09:39 -0400

    Implement home navigation with sub views

--- Delectable/app/src/main/java/com/delectable/mobile/ui/home/fragment/HomeFragment.java
+++ Delectable/app/src/main/java/com/delectable/mobile/ui/home/fragment/HomeFragment.java
@@ -1,45 +1,54 @@
 package com.delectable.mobile.ui.home.fragment;
 
 import com.delectable.mobile.R;
-import com.delectable.mobile.data.UserInfo;
 import com.delectable.mobile.ui.BaseFragment;
-import com.delectable.mobile.ui.registration.activity.LoginActivity;
+import com.delectable.mobile.ui.common.widget.SlidingPagerTabStrip;
+import com.delectable.mobile.ui.common.widget.TabsImagePagerAdapter;
 
-import android.content.Intent;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
-/**
- * Created by abednarek on 5/22/14.
- */
+import java.util.ArrayList;
+
 public class HomeFragment extends BaseFragment {
 
-    Button mLogoutButton;
+    private View mView;
 
-    public HomeFragment() {
-    }
+    private ViewPager mViewPager;
+
+    private SlidingPagerTabStrip mTabStrip;
+
+    private TabsImagePagerAdapter mTabsAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-        mLogoutButton = (Button) rootView.findViewById(R.id.mLogoutButton);
+        mView = inflater.inflate(R.layout.fragment_home, container, false);
 
-        // TODO: Temp logout button (Porobably shoud put logout functionality into BaseFragment/Activity?)
-        mLogoutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                UserInfo.onSignOut(getActivity());
-                Intent launchIntent = new Intent();
-                launchIntent.setClass(getActivity(), LoginActivity.class);
-                startActivity(launchIntent);
-                getActivity().finish();
-            }
-        });
-        // TODO: Build out menu system
-        return rootView;
+        mViewPager = (ViewPager) mView.findViewById(R.id.pager);
+        mTabStrip = (SlidingPagerTabStrip) mView.findViewById(R.id.tabstrip);
+
+        ArrayList<Integer> imageId = new ArrayList<Integer>();
+        ArrayList<Fragment> tabFragments = new ArrayList<Fragment>();
+
+        imageId.add(R.drawable.ab_feed);
+        tabFragments.add(FollowFeedTabFragment.newInstance());
+
+        imageId.add(R.drawable.ab_profile);
+        tabFragments.add(UserProfileTabFragment.newInstance());
+
+        imageId.add(R.drawable.ab_activity);
+        tabFragments.add(ActivityFeedTabFragment.newInstance());
+
+        mTabsAdapter = new TabsImagePagerAdapter(getFragmentManager(), tabFragments, imageId);
+
+        mViewPager.setAdapter(mTabsAdapter);
+        mTabStrip.setViewPager(mViewPager);
+
+        return mView;
     }
 }
