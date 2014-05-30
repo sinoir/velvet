0e7901fa6b19f57b3e120ad5de2ad15b7e3b6406
Adam Bednarek <adam@ad60.com>
2014-05-30 18:32:30 -0400

    WIP Start stubbing in User Profile screen

--- Delectable/app/src/main/java/com/delectable/mobile/ui/home/fragment/UserProfileTabFragment.java
+++ Delectable/app/src/main/java/com/delectable/mobile/ui/home/fragment/UserProfileTabFragment.java
@@ -3,14 +3,20 @@ package com.delectable.mobile.ui.home.fragment;
 import com.delectable.mobile.R;
 import com.delectable.mobile.ui.BaseFragment;
 
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 public class UserProfileTabFragment extends BaseFragment {
 
     private View mView;
+
+    private ListView mListView;
 
     public UserProfileTabFragment() {
         // Required empty public constructor
@@ -28,6 +34,14 @@ public class UserProfileTabFragment extends BaseFragment {
             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         mView = inflater.inflate(R.layout.fragment_home_user_profile_tab, container, false);
+
+        mListView = (ListView) mView.findViewById(R.id.list_view);
+
+        View profileHeader = inflater.inflate(R.layout.profile_header, null);
+        mListView.addHeaderView(profileHeader);
+
+        // TODO: Create/Add Adapter ...
+
         return mView;
     }
 }
