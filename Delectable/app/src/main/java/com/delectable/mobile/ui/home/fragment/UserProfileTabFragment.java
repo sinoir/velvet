49a9fccb12519da537016be002fccd84fc7889ef
Adam Bednarek <adam@ad60.com>
2014-05-30 12:07:08 -0400

    Stub Home nag screens/classes

---
+++ Delectable/app/src/main/java/com/delectable/mobile/ui/home/fragment/UserProfileTabFragment.java
@@ -0,0 +1,33 @@
+package com.delectable.mobile.ui.home.fragment;
+
+import com.delectable.mobile.R;
+import com.delectable.mobile.ui.BaseFragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class UserProfileTabFragment extends BaseFragment {
+
+    private View mView;
+
+    public UserProfileTabFragment() {
+        // Required empty public constructor
+    }
+
+    public static UserProfileTabFragment newInstance() {
+        UserProfileTabFragment fragment = new UserProfileTabFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mView = inflater.inflate(R.layout.fragment_home_user_profile_tab, container, false);
+        return mView;
+    }
+}
