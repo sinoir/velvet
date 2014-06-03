f2905885e3d9913462e0b9563253d22056a59315
Adam Bednarek <adam@ad60.com>
2014-06-03 14:03:37 -0400

    Add Menu to home screen
    - Capture icon / button
    - Sign out in menu

--- Delectable/app/src/main/java/com/delectable/mobile/ui/home/fragment/HomeFragment.java
+++ Delectable/app/src/main/java/com/delectable/mobile/ui/home/fragment/HomeFragment.java
@@ -1,16 +1,23 @@
 package com.delectable.mobile.ui.home.fragment;
 
 import com.delectable.mobile.R;
+import com.delectable.mobile.data.UserInfo;
 import com.delectable.mobile.ui.BaseFragment;
 import com.delectable.mobile.ui.common.widget.SlidingPagerTabStrip;
 import com.delectable.mobile.ui.common.widget.TabsImagePagerAdapter;
+import com.delectable.mobile.ui.registration.activity.LoginActivity;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -27,6 +34,9 @@ public class HomeFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
+
+        setHasOptionsMenu(true);
+
         mView = inflater.inflate(R.layout.fragment_home, container, false);
 
         mViewPager = (ViewPager) mView.findViewById(R.id.pager);
@@ -50,5 +60,33 @@ public class HomeFragment extends BaseFragment {
         mTabStrip.setViewPager(mViewPager);
 
         return mView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.home_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.capture_wine:
+                Toast.makeText(getActivity(), "Launch Capture Wine!", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.sign_out:
+                logout();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void logout() {
+        UserInfo.onSignOut(getActivity());
+        Intent launchIntent = new Intent();
+        launchIntent.setClass(getActivity(), LoginActivity.class);
+        startActivity(launchIntent);
+        getActivity().finish();
     }
 }
