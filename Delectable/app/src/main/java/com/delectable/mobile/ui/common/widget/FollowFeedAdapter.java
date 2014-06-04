a558dc4e5beaccd9d347072c37e9218f002b4695
Adam Bednarek <adam@ad60.com>
2014-06-04 11:33:57 -0400

    Display capturer rating / details
    - Follower feed : update with capturer comment, rating and name.

--- Delectable/app/src/main/java/com/delectable/mobile/ui/common/widget/FollowFeedAdapter.java
+++ Delectable/app/src/main/java/com/delectable/mobile/ui/common/widget/FollowFeedAdapter.java
@@ -3,8 +3,11 @@ package com.delectable.mobile.ui.common.widget;
 import com.delectable.mobile.R;
 import com.delectable.mobile.api.models.Account;
 import com.delectable.mobile.api.models.Capture;
+import com.delectable.mobile.api.models.CaptureComment;
 import com.delectable.mobile.api.models.CaptureDetails;
 import com.delectable.mobile.util.ImageLoaderUtil;
+
+import org.ocpsoft.prettytime.PrettyTime;
 
 import android.app.Activity;
 import android.view.LayoutInflater;
@@ -65,6 +68,15 @@ public class FollowFeedAdapter extends BaseAdapter {
             viewHolder.withText = rowView.findViewById(R.id.with_text);
             viewHolder.participantsImageContainer = (RelativeLayout) rowView
                     .findViewById(R.id.participants_image_container);
+
+            viewHolder.profileImage2 = (CircleImageView) rowView.findViewById(R.id.profile_image2);
+            viewHolder.userName = (TextView) rowView.findViewById(R.id.user_name);
+            viewHolder.userComment = (TextView) rowView.findViewById(R.id.user_comment);
+            viewHolder.captureTimeLocation = (TextView) rowView
+                    .findViewById(R.id.capture_time_location);
+            viewHolder.userCaptureRatingBar = (RatingsBarView) rowView
+                    .findViewById(R.id.capturer_rating_bar);
+
             rowView.setTag(viewHolder);
         } else {
             viewHolder = (FeedViewHolder) rowView.getTag();
@@ -75,6 +87,11 @@ public class FollowFeedAdapter extends BaseAdapter {
         String wineName = "";
         String profileImageUrl = getThumbnailParticipantPhotoFromAccount(
                 capture.getCapturerParticipant());
+        String userName = "";
+        String userComment = "";
+        String captureTimeLocation = "";
+        String userAccountId = "";
+        float capturePercent = 0.0f;
 
         boolean hasCaptureParticipants = (capture.getCapturerParticipants() != null
                 && capture.getCapturerParticipants().size() > 0);
@@ -85,12 +102,43 @@ public class FollowFeedAdapter extends BaseAdapter {
             wineImageUrl = capture.getPhoto().getUrl();
         }
 
+        if (capture.getCapturerParticipant() != null) {
+            userName = capture.getCapturerParticipant().getFullName();
+            userAccountId = capture.getCapturerParticipant().getId();
+        }
+
+        CaptureComment userCaptureComment = capture.getCommentForUserId(userAccountId);
+        if (userCaptureComment != null) {
+            userComment = userCaptureComment.getComment();
+        }
+
+        capturePercent = capture.getRatingPercentForId(userAccountId);
+
+        PrettyTime p = new PrettyTime();
+        captureTimeLocation = p.format(capture.getCreatedAtDate());
+
+        // TODO: Add Location once location is added to model
+        String location = "";
+        captureTimeLocation += location;
+
         ImageLoaderUtil.loadImageIntoView(mContext, wineImageUrl, viewHolder.wineImage);
         ImageLoaderUtil.loadImageIntoView(mContext, profileImageUrl, viewHolder.profileImage1);
+        ImageLoaderUtil.loadImageIntoView(mContext, profileImageUrl, viewHolder.profileImage2);
 
         viewHolder.producerName.setText(producerName);
         viewHolder.wineName.setText(wineName);
+        viewHolder.userName.setText(userName);
+        if (userComment != "") {
+            viewHolder.userComment.setText(userComment);
+            viewHolder.userComment.setVisibility(View.VISIBLE);
+        } else {
+            viewHolder.userComment.setVisibility(View.GONE);
+        }
+        viewHolder.userCaptureRatingBar.setPercent(capturePercent);
 
+        viewHolder.captureTimeLocation.setText(captureTimeLocation);
+
+        // TODO: Unhide the capturer_with_container view once figured out
         if (hasCaptureParticipants) {
             viewHolder.withText.setVisibility(View.VISIBLE);
             viewHolder.participantsImageContainer.setVisibility(View.VISIBLE);
@@ -98,6 +146,8 @@ public class FollowFeedAdapter extends BaseAdapter {
             viewHolder.withText.setVisibility(View.INVISIBLE);
             viewHolder.participantsImageContainer.setVisibility(View.GONE);
         }
+
+        // TODO : Dynamic list of other comments ..
 
         return rowView;
     }
@@ -136,5 +186,15 @@ public class FollowFeedAdapter extends BaseAdapter {
         View withText;
 
         RelativeLayout participantsImageContainer;
+
+        CircleImageView profileImage2;
+
+        TextView userName;
+
+        TextView userComment;
+
+        TextView captureTimeLocation;
+
+        RatingsBarView userCaptureRatingBar;
     }
 }
