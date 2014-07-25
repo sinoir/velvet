9a8fbf6ffeb3ddfe95d2f49cc0a4e76d7e952c4b
Adam Bednarek <adam@ad60.com>
2014-07-25 10:37:19 -0400

    BaseListingElement
    - Create a base listing element and extend it in capture.
    - Should be used anywhere we use a listing with before / after and has a create date and id.

--- Delectable/app/src/main/java/com/delectable/mobile/api/models/Capture.java
+++ Delectable/app/src/main/java/com/delectable/mobile/api/models/Capture.java
@@ -2,48 +2,12 @@ package com.delectable.mobile.api.models;
 
 import com.google.gson.annotations.SerializedName;
 
-import java.util.Comparator;
-
-public abstract class Capture extends BaseResponse {
-
-    /**
-     * Sorts Captures by newest to oldest
-     */
-    public static Comparator<Capture> CreatedAtDescendingComparator = new Comparator<Capture>() {
-
-        @Override
-        public int compare(Capture lhs, Capture rhs) {
-            if (!(lhs instanceof Capture) || !(rhs instanceof Capture)) {
-                throw new ClassCastException();
-            }
-            return (int) (rhs.getCreatedAt() - lhs.getCreatedAt());
-        }
-    };
-
-    String id;
-
-    Double created_at;
+public abstract class Capture extends BaseListingElement {
 
     @SerializedName("private")
     Boolean private_;
 
     Account capturer_participant;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Double getCreatedAt() {
-        return created_at;
-    }
-
-    public void setCreatedAt(Double created_at) {
-        this.created_at = created_at;
-    }
 
     public Boolean getPrivate() {
         return private_;
@@ -64,10 +28,8 @@ public abstract class Capture extends BaseResponse {
     @Override
     public String toString() {
         return "Capture{" +
-                "id='" + id + '\'' +
-                ", created_at=" + created_at +
-                ", private_=" + private_ +
+                "private_=" + private_ +
                 ", capturer_participant=" + capturer_participant +
-                "} " + super.toString();
+                '}';
     }
 }
