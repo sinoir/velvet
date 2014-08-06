72ec2746a9f8e8117c1c084602e18523d09b6f6f
Wayne Chiu <wayne@delectable.com>
2014-08-05 18:36:17 -0700

    made AccountsProvisionProfilePhotoRequest

---
+++ Delectable/app/src/main/java/com/delectable/mobile/api/requests/AccountsProvisionProfilePhotoRequest.java
@@ -0,0 +1,27 @@
+package com.delectable.mobile.api.requests;
+
+import com.delectable.mobile.api.models.Account;
+import com.delectable.mobile.api.models.ProvisionCapture;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class AccountsProvisionProfilePhotoRequest extends BaseRequest {
+
+    @Override
+    public String[] getPayloadFields() {
+        //build empty payload
+        return new String[0];
+    }
+
+    @Override
+    public String getResourceUrl() {
+        return API_VER + "/accounts/provision_profile_photo";
+    }
+
+    @Override
+    public ProvisionCapture buildResopnseFromJson(JSONObject jsonObject) {
+        return ProvisionCapture.buildFromJson(jsonObject);
+    }
+}
