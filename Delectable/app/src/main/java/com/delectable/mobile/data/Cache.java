7b33b828ec303369a362db7e53765d6dae3da249
Henning Post <henningpost@gmail.com>
2014-08-07 15:08:57 -0700

    Increased cache size to 10 megabytes (unit is bytes, not kilobytes as ObjectCache implies within their constructor)

--- Delectable/app/src/main/java/com/delectable/mobile/data/Cache.java
+++ Delectable/app/src/main/java/com/delectable/mobile/data/Cache.java
@@ -1,18 +1,18 @@
 package com.delectable.mobile.data;
 
-import android.content.Context;
-import android.util.Log;
-
 import com.delectable.mobile.BuildConfig;
 import com.iainconnor.objectcache.CacheManager;
 import com.iainconnor.objectcache.DiskCache;
+
+import android.content.Context;
+import android.util.Log;
 
 import java.io.File;
 import java.io.IOException;
 
 public class Cache {
 
-    private static final int CACHE_SIZE_KB = 1024 * 10;
+    private static final int CACHE_SIZE_BYTES = 1024 * 1024 * 10;
 
     private static CacheManager sCacheManager;
     private static DiskCache sDiskCache;
@@ -28,7 +28,7 @@ public class Cache {
         File cacheFile = new File(cachePath + File.separator + BuildConfig.PACKAGE_NAME);
 
         try {
-            sDiskCache = new DiskCache(cacheFile, BuildConfig.VERSION_CODE, CACHE_SIZE_KB);
+            sDiskCache = new DiskCache(cacheFile, BuildConfig.VERSION_CODE, CACHE_SIZE_BYTES);
             sCacheManager = CacheManager.getInstance(sDiskCache);
         } catch (IOException e) {
             Log.e(TAG, "cache init failed: " + e.getMessage());
