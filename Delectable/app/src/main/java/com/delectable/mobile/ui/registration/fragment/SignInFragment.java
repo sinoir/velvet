eca3ae075ae1471ff9c8a963984c903122ceaecb
Wayne Chiu <wayne@delectable.com>
2014-08-13 18:35:25 -0700

    laid out SignIn and SignUp fragment foundation
    - no API calls yet
    - connected both to LandingPageFragment

---
+++ Delectable/app/src/main/java/com/delectable/mobile/ui/registration/fragment/SignInFragment.java
@@ -0,0 +1,103 @@
+package com.delectable.mobile.ui.registration.fragment;
+
+import com.delectable.mobile.R;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.OnClick;
+
+public class SignInFragment extends BaseSignUpInFragment {
+
+    private static final String TAG = SignInFragment.class.getSimpleName();
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+
+        getTitleTextView().setText(R.string.sign_in_title);
+        getDoneButton().setEnabled(false);
+        getNameFieldContainer().setVisibility(View.GONE);
+        getFacebookTextView().setText(R.string.signup_in_sign_in_using_facebook);
+        getGoogleTextView().setText(R.string.signup_in_sign_in_using_google);
+        getTermsPrivacyContainer().setVisibility(View.GONE);
+
+        getEmailField().addTextChangedListener(TextValidationWatcher);
+        getPasswordField().addTextChangedListener(TextValidationWatcher);
+        return rootView;
+    }
+
+    /**
+     * Sets whether the done button is enabled or not depending on whether the fields are all filled out.
+     */
+    private TextWatcher TextValidationWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            Log.d(TAG, "afterTextChanged: " + s.toString());
+            //only enable done button if all fields are filled
+            if (emptyFieldExists()) {
+                getDoneButton().setEnabled(false);
+                return;
+            }
+            getDoneButton().setEnabled(true);
+        }
+    };
+
+    private boolean emptyFieldExists() {
+        if(getEmailField().getText().toString().trim().equals("")) {
+            return true;
+        }
+        if(getPasswordField().getText().toString().trim().equals("")) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected void onKeyboardDoneButtonClick() {
+        onDoneButtonClick();
+    }
+
+    @Override
+    protected void onDoneButtonClick() {
+        if (emptyFieldExists()) {
+            return;
+        }
+        String email = getEmailField().getText().toString().trim();
+        String password = getPasswordField().getText().toString().trim();
+    }
+
+    @OnClick(R.id.forgot_textview)
+    protected void onForgotTextClick() {
+        Log.d(TAG, "onForgotTextClick");
+    }
+
+    @Override
+    protected void onFacebookButtonClick() {
+        Log.d(TAG, "onFacebookButtonClick");
+
+    }
+
+    @Override
+    protected void onGoogleButtonClick() {
+        Log.d(TAG, "onGoogleButtonClick");
+
+
+    }
+
+
+}
---
+++ Delectable/app/src/main/java/com/delectable/mobile/ui/registration/fragment/SignUpFragment.java
@@ -0,0 +1,114 @@
+package com.delectable.mobile.ui.registration.fragment;
+
+import com.delectable.mobile.R;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.OnClick;
+
+public class SignUpFragment extends BaseSignUpInFragment {
+
+    private static final String TAG = SignUpFragment.class.getSimpleName();
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        getTitleTextView().setText(R.string.sign_up_title);
+        getDoneButton().setEnabled(false);
+        getForgotTextView().setVisibility(View.GONE);
+        getFacebookTextView().setText(R.string.signup_in_sign_up_using_facebook);
+        getGoogleTextView().setText(R.string.signup_in_sign_up_using_google);
+
+
+        getNameField().addTextChangedListener(TextValidationWatcher);
+        getEmailField().addTextChangedListener(TextValidationWatcher);
+        getPasswordField().addTextChangedListener(TextValidationWatcher);
+        return rootView;
+    }
+
+    /**
+     * Sets whether the done button is enabled or not depending on whether the fields are all filled out.
+     */
+    private TextWatcher TextValidationWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            Log.d(TAG, "afterTextChanged: " + s.toString());
+            //only enable done button if all fields are filled
+            if (emptyFieldExists()) {
+                getDoneButton().setEnabled(false);
+                return;
+            }
+            getDoneButton().setEnabled(true);
+        }
+    };
+
+    private boolean emptyFieldExists() {
+        if(getNameField().getText().toString().trim().equals("")) {
+            return true;
+        }
+        if(getEmailField().getText().toString().trim().equals("")) {
+            return true;
+        }
+        if(getPasswordField().getText().toString().trim().equals("")) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected void onKeyboardDoneButtonClick() {
+        onDoneButtonClick();
+    }
+
+    @Override
+    protected void onDoneButtonClick() {
+        if (emptyFieldExists()) {
+            return;
+        }
+        String name = getNameField().getText().toString().trim();
+        String email = getEmailField().getText().toString().trim();
+        String password = getPasswordField().getText().toString().trim();
+    }
+
+    @Override
+    protected void onFacebookButtonClick() {
+        Log.d(TAG, "onFacebookButtonClick");
+
+    }
+
+    @Override
+    protected void onGoogleButtonClick() {
+        Log.d(TAG, "onGoogleButtonClick");
+        //TODO Google sign up
+    }
+
+    @OnClick(R.id.terms_textview)
+    protected void goToTermsOfUse() {
+        Log.d(TAG, "goToTermsOfUse");
+        //TODO need link/text for terms of use
+
+    }
+
+    @OnClick(R.id.privacy_textview)
+    protected void goToPrivacyPolicy() {
+        Log.d(TAG, "goToPrivacyPolicy");
+        //TODO need link/text for privacy policy
+    }
+
+
+}
