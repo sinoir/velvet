4e46f7b1177b9b264cc6a1d58ec0eea830dfe672
Wayne Chiu <wayne@delectable.com>
2014-08-15 10:06:51 -0700

    Added Error object to BaseResponse
    - still need to make other fields private

--- Delectable/app/src/main/java/com/delectable/mobile/model/api/BaseResponse.java
+++ Delectable/app/src/main/java/com/delectable/mobile/model/api/BaseResponse.java
@@ -3,8 +3,48 @@ package com.delectable.mobile.model.api;
 public class BaseResponse {
 
     public String e_tag;
+
     public boolean e_tag_match;
+
     public String context;
+
     public boolean success;
+
+    private Error error;
+
+    public String getETag() {
+        return e_tag;
+    }
+
+    public boolean isETagMatch() {
+        return e_tag_match;
+    }
+
+    public String getContext() {
+        return context;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public Error getError() {
+        return error;
+    }
+
+    public static class Error {
+
+        private int code;
+
+        private String message;
+
+        public int getCode() {
+            return code;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
 
 }
