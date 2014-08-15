f9c272b6cbba9eb5732c69be3c7001645c73ab86
Wayne Chiu <wayne@delectable.com>
2014-08-15 10:17:08 -0700

    improvements to NetworkClient
    - handles API errors now, passing them up through exceptions
    - more verbose logging: Logs url, request body, response body now
    - more detailed error messages passed up through exceptions
    -  added an authenticate boolean parameter, bc register and login shouldn't pass up sessionKey and sessionToken
    - made convenience post() method that handles passing in authenticate true, all other API calls use this method

--- Delectable/app/src/main/java/com/delectable/mobile/net/NetworkClient.java
+++ Delectable/app/src/main/java/com/delectable/mobile/net/NetworkClient.java
@@ -1,32 +1,58 @@
 package com.delectable.mobile.net;
 
+import com.google.gson.Gson;
+
 import com.delectable.mobile.App;
 import com.delectable.mobile.Config;
 import com.delectable.mobile.data.UserInfo;
 import com.delectable.mobile.model.api.BaseRequest;
 import com.delectable.mobile.model.api.BaseResponse;
-import com.google.gson.Gson;
 import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
 
+import android.util.Log;
+
 import java.io.IOException;
 
 public class NetworkClient {
 
+    private static final String TAG = NetworkClient.class.getSimpleName();
+
+
     private static final String USER_AGENT = "DelectableAndroid";
+
     private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
     private OkHttpClient mClient = new OkHttpClient();
+
     private Gson mGson = new Gson();
 
-    public <T extends BaseResponse> T post(String endpoint, BaseRequest requestObject, Class<T> responseClass) throws IOException {
+    /**
+     * Convenience method that calls {@link #post(String, BaseRequest, Class, boolean)} with
+     * authentication set to true. Generally all API requests call this method except for
+     * registrations/register and registrations/login.
+     */
+    public <T extends BaseResponse> T post(String endpoint, BaseRequest requestObject,
+            Class<T> responseClass) throws IOException {
+        return post(endpoint, requestObject, responseClass, true);
+    }
 
-        authenticateRequest(requestObject);
+    /**
+     * @param authenticate <code>true</code> if you want to pass in sessionKey and sessionToken with
+     *                     the request.
+     */
+    public <T extends BaseResponse> T post(String endpoint, BaseRequest requestObject,
+            Class<T> responseClass, boolean authenticate) throws IOException {
+        if (authenticate) {
+            authenticateRequest(requestObject);
+        }
         String requestString = mGson.toJson(requestObject);
-
+        String requestName = TAG + "." + requestObject.getClass().getSimpleName();
+        Log.i(requestName, "url: " + getAbsolutePath() + endpoint);
+        Log.i(requestName, "body: " + requestString);
         RequestBody body = RequestBody.create(JSON, requestString);
         Request request = new Request.Builder()
                 .url(getAbsolutePath() + endpoint)
@@ -35,12 +61,35 @@ public class NetworkClient {
                 .build();
         Response response = mClient.newCall(request).execute();
 
+        //handle HTTP errors
         if (!response.isSuccessful()) {
             // TODO error handling, throw exception that gets handled in job queue
-            throw new IOException("request not successfull: " + response);
+            throw new IOException(
+                    "Request Error " + response.code() + ": " + response.toString());
+        }
+        //be careful, calling this ResponseBody.string() method more than once wipes out the string
+        String responseBody = response.body().string();
+        Log.i(requestName, "response: " + responseBody);
+        T responseObj = mGson.fromJson(responseBody, responseClass);
+
+        //handle Gson parsing errors
+        if (responseObj == null) {
+            throw new IOException("Error parsing response into JSON");
         }
 
-        return mGson.fromJson(response.body().string(), responseClass);
+        //handle API errors
+        if (!responseObj.success) {
+
+            if (responseObj.getError() == null) {
+                throw new IOException("API Error with no message.");
+            }
+
+            throw new IOException(
+                    "Error " + responseObj.getError().getCode() + ": " + responseObj.getError()
+                            .getMessage());
+        }
+
+        return responseObj;
     }
 
     private String getAbsolutePath() {
