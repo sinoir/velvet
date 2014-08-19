51f578b13b13a4bee1fdfe21994cd4b7654b588f
Wayne Chiu <wayne@delectable.com>
2014-08-18 17:27:54 -0700

    added email prepopulation to BaseSignUpInFragment
    - also added region markers

--- Delectable/app/src/main/java/com/delectable/mobile/ui/registration/fragment/BaseSignUpInFragment.java
+++ Delectable/app/src/main/java/com/delectable/mobile/ui/registration/fragment/BaseSignUpInFragment.java
@@ -13,9 +13,15 @@ import com.facebook.SessionState;
 import com.facebook.UiLifecycleHelper;
 import com.facebook.widget.LoginButton;
 
+import android.app.LoaderManager;
 import android.content.Context;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -29,6 +35,9 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Inject;
 
 import butterknife.ButterKnife;
@@ -39,11 +48,15 @@ import butterknife.OnClick;
  * Sign up and Sign in screens both share the same xml view, this base class takes care of
  * butterknifing the views.
  */
-public abstract class BaseSignUpInFragment extends BaseFragment {
+public abstract class BaseSignUpInFragment extends BaseFragment
+        implements LoaderManager.LoaderCallbacks<Cursor> {
 
     //not making this TAG static in order to get the concrete class'name to show up in Logs
     private final String TAG = this.getClass().getSimpleName();
 
+    private static final int LOADER_EMAIL_AUTOCOMPLETE = 0;
+
+    //region Views
     @InjectView(R.id.sign_up_sign_in_title)
     protected FontTextView mTitleTextView;
 
@@ -66,7 +79,8 @@ public abstract class BaseSignUpInFragment extends BaseFragment {
     protected FontTextView mForgotTextView;
 
     /**
-     * This is merely the UI button, clicking this will programmatically invoke a click on the real facebook button.
+     * This is merely the UI button, clicking this will programmatically invoke a click on the real
+     * facebook button.
      */
     @InjectView(R.id.facebook_button)
     protected RelativeLayout mFacebookButton;
@@ -85,12 +99,20 @@ public abstract class BaseSignUpInFragment extends BaseFragment {
 
     @InjectView(R.id.terms_privacy_container)
     protected LinearLayout mTermsPrivacyContainer;
+    //endregion
 
     private UiLifecycleHelper mFacebookUiHelper;
+
+    /**
+     * The default email gotten from the phone, that we use to prepopulate the email field here and
+     * in {@link com.delectable.mobile.ui.registration.dialog.ResetPasswordDialog}
+     */
+    private String mPhoneEmail;
 
     @Inject
     RegistrationController mRegistrationController;
 
+    //region Lifecycle
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -117,6 +139,7 @@ public abstract class BaseSignUpInFragment extends BaseFragment {
     public void onResume() {
         super.onResume();
         mFacebookUiHelper.onResume();
+        getLoaderManager().initLoader(LOADER_EMAIL_AUTOCOMPLETE, null, this);
     }
 
     @Override
@@ -142,7 +165,57 @@ public abstract class BaseSignUpInFragment extends BaseFragment {
         super.onSaveInstanceState(outState);
         mFacebookUiHelper.onSaveInstanceState(outState);
     }
+    //endregion
 
+    //region Email Prepopulation
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle arguments) {
+        return new CursorLoader(getActivity(),
+                // Retrieve data rows for the device user's 'profile' contact.
+                Uri.withAppendedPath(
+                        ContactsContract.Profile.CONTENT_URI,
+                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY),
+                ProfileQuery.PROJECTION,
+
+                // Select only email addresses.
+                ContactsContract.Contacts.Data.MIMETYPE + " = ?",
+                new String[]{ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE},
+
+                // Show primary email addresses first. Note that there won't be
+                // a primary email address if the user hasn't specified one.
+                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        List<String> emails = new ArrayList<String>();
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            emails.add(cursor.getString(ProfileQuery.ADDRESS));
+            // Potentially filter on ProfileQuery.IS_PRIMARY
+            cursor.moveToNext();
+        }
+        mPhoneEmail = emails.get(0);
+        mEmailField.setText(mPhoneEmail);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+    }
+
+    private interface ProfileQuery {
+
+        String[] PROJECTION = {
+                ContactsContract.CommonDataKinds.Email.ADDRESS,
+                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
+        };
+
+        int ADDRESS = 0;
+        int IS_PRIMARY = 1;
+    }
+    //endregion
+
+    //region Button Action Listeners
     /**
      * Listens for done button on soft keyboard.
      */
@@ -180,8 +253,9 @@ public abstract class BaseSignUpInFragment extends BaseFragment {
 
     @OnClick(R.id.google_button)
     protected abstract void onGoogleButtonClick();
+    //endregion
 
-
+    //region Getters
     public FontTextView getTitleTextView() {
         return mTitleTextView;
     }
@@ -234,10 +308,13 @@ public abstract class BaseSignUpInFragment extends BaseFragment {
         return mRealFacebookButton;
     }
 
+    public String getPhoneEmail() {
+        return mPhoneEmail;
+    }
+    //endregion
 
     public void onEventMainThread(LoginRegisterEvent registerEvent) {
         if (registerEvent.isSuccessful()) {
-            Log.d(this.getClass().getSimpleName(), "Successfully Logged in!");
             startActivity(new Intent(getActivity(), NavActivity.class));
             getActivity().finish();
             return;
