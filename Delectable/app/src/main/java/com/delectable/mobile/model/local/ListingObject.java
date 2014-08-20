26355edcab49dc4052742a7a8923ec59ec66cc8f
Adam Bednarek <adam@ad60.com>
2014-08-20 14:01:25 -0400

    Create listing object used for cache
    - Create a listing object that will be used for storing the data locally, it's meant to cache only the IDs, not the actual capture objects.
    - The item objects will be stored separately.

---
+++ Delectable/app/src/main/java/com/delectable/mobile/model/local/ListingObject.java
@@ -0,0 +1,71 @@
+package com.delectable.mobile.model.local;
+
+import com.delectable.mobile.api.models.CaptureDetails;
+import com.delectable.mobile.api.models.ListingResponse;
+
+import java.util.ArrayList;
+
+/**
+ * Listing object storing IDs for specific objects
+ */
+public class ListingObject {
+
+    String e_tag;
+
+    Boolean more;
+
+    boolean invalidate;
+
+    ListingResponse.Boundaries boundaries;
+
+    ArrayList<String> object_ids;
+
+    public ListingObject(ListingResponse<CaptureDetails> captureDetails) {
+        e_tag = captureDetails.getETag();
+        more = captureDetails.getMore();
+        invalidate = captureDetails.getInvalidate();
+        boundaries = captureDetails.getBoundaries();
+        object_ids = new ArrayList<String>();
+        object_ids.addAll(captureDetails.getAllIds());
+    }
+
+    public String getETag() {
+        return e_tag;
+    }
+
+    public void setETag(String e_tag) {
+        this.e_tag = e_tag;
+    }
+
+    public Boolean getMore() {
+        return more;
+    }
+
+    public void setMore(Boolean more) {
+        this.more = more;
+    }
+
+    public boolean isInvalidate() {
+        return invalidate;
+    }
+
+    public void setInvalidate(boolean invalidate) {
+        this.invalidate = invalidate;
+    }
+
+    public ListingResponse.Boundaries getBoundaries() {
+        return boundaries;
+    }
+
+    public void setBoundaries(ListingResponse.Boundaries boundaries) {
+        this.boundaries = boundaries;
+    }
+
+    public ArrayList<String> getObjectIds() {
+        return object_ids;
+    }
+
+    public void setObjectIds(ArrayList<String> objectIds) {
+        this.object_ids = objectIds;
+    }
+}
