fbcddf845a8fb3c21306f27c282b795169c5e517
Wayne Chiu <wayne@delectable.com>
2014-08-21 18:22:37 -0700

    API stubs for FollowExpertsTabFragment

--- Delectable/app/src/main/java/com/delectable/mobile/ui/followfriends/fragment/FollowExpertsTabFragment.java
+++ Delectable/app/src/main/java/com/delectable/mobile/ui/followfriends/fragment/FollowExpertsTabFragment.java
@@ -1,16 +1,32 @@
 package com.delectable.mobile.ui.followfriends.fragment;
 
+import com.delectable.mobile.App;
+import com.delectable.mobile.controllers.AccountController;
+import com.delectable.mobile.events.accounts.FetchInfluencerSuggestionsEvent;
 import com.delectable.mobile.ui.BaseFragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class FollowExpertsTabFragment extends BaseFragment{
+import javax.inject.Inject;
+
+public class FollowExpertsTabFragment extends BaseFragment {
+
+    @Inject
+    AccountController mAccountController;
 
     private static final String TAG = FollowExpertsTabFragment.class.getSimpleName();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        App.injectMembers(this);
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -19,6 +35,21 @@ public class FollowExpertsTabFragment extends BaseFragment{
         TextView tv = new TextView(getActivity());
         tv.setText(TAG);
         return tv;
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mAccountController.fetchInfluencerSuggestions();
+    }
+
+    public void onEventMainThread(FetchInfluencerSuggestionsEvent event) {
+        if (event.isSuccessful()) {
+            Log.d(TAG, "fetch success! account retrieved: " + event.getAccounts().size());
+            return;
+        }
+        Log.d(TAG, "event failed");
 
     }
 }
