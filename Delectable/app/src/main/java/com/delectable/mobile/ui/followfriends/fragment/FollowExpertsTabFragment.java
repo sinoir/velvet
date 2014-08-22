c394c83217a4b0e3bf643549405bf0ac50999aa3
Wayne Chiu <wayne@delectable.com>
2014-08-22 10:43:03 -0700

    InfluencerAccountsAdapter implemented
    - successfully got rows up on screen!
    - still need to handle follow functionality, caching?, holding onto follow status of an account

--- Delectable/app/src/main/java/com/delectable/mobile/ui/followfriends/fragment/FollowExpertsTabFragment.java
+++ Delectable/app/src/main/java/com/delectable/mobile/ui/followfriends/fragment/FollowExpertsTabFragment.java
@@ -1,55 +1,77 @@
 package com.delectable.mobile.ui.followfriends.fragment;
 
 import com.delectable.mobile.App;
+import com.delectable.mobile.R;
+import com.delectable.mobile.api.models.AccountMinimal;
 import com.delectable.mobile.controllers.AccountController;
 import com.delectable.mobile.events.accounts.FetchInfluencerSuggestionsEvent;
 import com.delectable.mobile.ui.BaseFragment;
+import com.delectable.mobile.ui.followfriends.widget.FollowExpertsRow;
+import com.delectable.mobile.ui.followfriends.widget.InfluencerAccountsAdapter;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import javax.inject.Inject;
 
-public class FollowExpertsTabFragment extends BaseFragment {
+public class FollowExpertsTabFragment extends BaseFragment
+        implements FollowExpertsRow.ActionsHandler {
 
     @Inject
     AccountController mAccountController;
 
     private static final String TAG = FollowExpertsTabFragment.class.getSimpleName();
 
+    private InfluencerAccountsAdapter mAdapter = new InfluencerAccountsAdapter(this);
+
+    private ArrayList<AccountMinimal> mAccounts;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         App.injectMembers(this);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-
-        TextView tv = new TextView(getActivity());
-        tv.setText(TAG);
-        return tv;
+        ListView listview = (ListView) inflater
+                .inflate(R.layout.fragment_listview, container, false);
+        listview.setAdapter(mAdapter);
+        return listview;
 
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        mAccountController.fetchInfluencerSuggestions();
+        if (mAccounts == null) {
+            mAccountController.fetchInfluencerSuggestions();
+        }
     }
 
     public void onEventMainThread(FetchInfluencerSuggestionsEvent event) {
         if (event.isSuccessful()) {
-            Log.d(TAG, "fetch success! account retrieved: " + event.getAccounts().size());
+            mAccounts = event.getAccounts();
+            mAdapter.setAccounts(event.getAccounts());
+            mAdapter.notifyDataSetChanged();
             return;
         }
-        Log.d(TAG, "event failed");
+        //event error
+        Toast.makeText(getActivity(), event.getErrorMessage(), Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void toggleFollow(AccountMinimal account, boolean isFollowing) {
+        Log.d(TAG, account.getFname() + " " + isFollowing);
 
     }
 }
