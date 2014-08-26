0100cc1e63740e88bb93d6873b8375923d3f6a13
Wayne Chiu <wayne@delectable.com>
2014-08-25 18:05:01 -0700

    added comment to AccountsInfluencerSuggestionsResponse

--- Delectable/app/src/main/java/com/delectable/mobile/model/api/accounts/AccountsInfluencerSuggestionsResponse.java
+++ Delectable/app/src/main/java/com/delectable/mobile/model/api/accounts/AccountsInfluencerSuggestionsResponse.java
@@ -1,21 +1,20 @@
 package com.delectable.mobile.model.api.accounts;
 
-import com.delectable.mobile.api.models.Account;
 import com.delectable.mobile.api.models.AccountMinimal;
 import com.delectable.mobile.model.api.BaseResponse;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
+/**
+ * The /accounts/facebook_suggestions endpoint also uses this response object.
+ */
 public class AccountsInfluencerSuggestionsResponse extends BaseResponse {
-
 
     private Payload payload;
 
     public Payload getPayload() {
         return payload;
     }
-
 
     public static class Payload {
 
@@ -25,6 +24,4 @@ public class AccountsInfluencerSuggestionsResponse extends BaseResponse {
             return accounts;
         }
     }
-
-
 }
