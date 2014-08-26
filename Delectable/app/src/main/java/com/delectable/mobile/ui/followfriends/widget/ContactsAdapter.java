645bcf740a18192f948b846898f96f542b5ecd71
Wayne Chiu <wayne@delectable.com>
2014-08-25 18:28:18 -0700

    changed over FollowContactRow to use merge tags

--- Delectable/app/src/main/java/com/delectable/mobile/ui/followfriends/widget/ContactsAdapter.java
+++ Delectable/app/src/main/java/com/delectable/mobile/ui/followfriends/widget/ContactsAdapter.java
@@ -1,7 +1,9 @@
 package com.delectable.mobile.ui.followfriends.widget;
 
+import com.delectable.mobile.R;
 import com.delectable.mobile.api.models.AccountMinimal;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -42,7 +44,9 @@ public class ContactsAdapter extends BaseAdapter {
 
         FollowContactRow row = (FollowContactRow) convertView;
         if (row == null) {
-            row = new FollowContactRow(parent.getContext());
+            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+            row = (FollowContactRow) inflater
+                    .inflate(R.layout.row_find_contact_with_sizing, parent, false);
             row.setActionsHandler(mActionsHandler);
         }
         row.updateData(getItem(position));
