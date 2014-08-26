6641a3fb84504889ad3bef289b6d76ff8ccce1e3
Adam Bednarek <adam@ad60.com>
2014-08-26 15:04:44 -0400

    Add Follow Contacts Fragment with contacts

--- Delectable/app/src/main/java/com/delectable/mobile/ui/followfriends/fragment/FollowContactsTabFragment.java
+++ Delectable/app/src/main/java/com/delectable/mobile/ui/followfriends/fragment/FollowContactsTabFragment.java
@@ -1,23 +1,68 @@
 package com.delectable.mobile.ui.followfriends.fragment;
 
-import com.delectable.mobile.ui.BaseFragment;
+import com.delectable.mobile.App;
+import com.delectable.mobile.R;
+import com.delectable.mobile.controllers.AccountController;
+import com.delectable.mobile.events.accounts.FetchedAccountsFromContactsEvent;
+import com.delectable.mobile.ui.followfriends.widget.BaseAccountsMinimalAdapter;
+import com.delectable.mobile.ui.followfriends.widget.ContactsAdapter;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ListView;
 
-public class FollowContactsTabFragment extends BaseFragment{
+import javax.inject.Inject;
+
+public class FollowContactsTabFragment extends BaseFollowFriendsTabFragment {
 
     private static final String TAG = FollowContactsTabFragment.class.getSimpleName();
+
+    @Inject
+    AccountController mAccountController;
+
+    private ContactsAdapter mAdapter = new ContactsAdapter(this);
+
+    @Override
+    protected BaseAccountsMinimalAdapter getAdapter() {
+        return mAdapter;
+    }
+
+    @Override
+    protected String getEventId() {
+        return TAG;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        App.injectMembers(this);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
+        ListView listview = (ListView) inflater
+                .inflate(R.layout.fragment_listview, container, false);
+        listview.setAdapter(mAdapter);
+        return listview;
+    }
 
-        TextView tv = new TextView(getActivity());
-        tv.setText(TAG);
-        return tv;
+    @Override
+    public void onResume() {
+        super.onResume();
+        mAccountController.fetchAccountsFromContacts();
+    }
+
+    public void onEventMainThread(FetchedAccountsFromContactsEvent event) {
+        if (event.isSuccessful()) {
+            mAccounts = event.getAccounts();
+            mAdapter.setAccounts(event.getAccounts());
+            mAdapter.notifyDataSetChanged();
+            return;
+        }
+        //event error
+        showToastError(event.getErrorMessage());
     }
 }
