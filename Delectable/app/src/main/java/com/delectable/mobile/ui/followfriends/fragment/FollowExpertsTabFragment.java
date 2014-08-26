e9d3b16f0cac1b06bfceb08966147656d247bb23
Wayne Chiu <wayne@delectable.com>
2014-08-25 19:41:05 -0700

    BaseFollowFriendsTabFragment
    - all the follow friend fragment screens are composed of the same underlying data (array of AccountMinimal) and the ability to follow an account, this base class handles all of that logic
    - FollowExperts and FollowFacebook both now subclass from this base class

--- Delectable/app/src/main/java/com/delectable/mobile/ui/followfriends/fragment/FollowExpertsTabFragment.java
+++ Delectable/app/src/main/java/com/delectable/mobile/ui/followfriends/fragment/FollowExpertsTabFragment.java
@@ -1,13 +1,6 @@
 package com.delectable.mobile.ui.followfriends.fragment;
 
-import com.delectable.mobile.App;
 import com.delectable.mobile.R;
-import com.delectable.mobile.api.models.AccountMinimal;
-import com.delectable.mobile.controllers.AccountController;
-import com.delectable.mobile.events.accounts.FetchFriendSuggestionsEvent;
-import com.delectable.mobile.events.accounts.FollowAccountEvent;
-import com.delectable.mobile.ui.BaseFragment;
-import com.delectable.mobile.ui.followfriends.widget.FollowActionsHandler;
 import com.delectable.mobile.ui.followfriends.widget.InfluencerAccountsAdapter;
 
 import android.os.Bundle;
@@ -16,38 +9,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
-import javax.inject.Inject;
-
-public class FollowExpertsTabFragment extends BaseFragment
-        implements FollowActionsHandler {
-
-    @Inject
-    protected AccountController mAccountController;
+public class FollowExpertsTabFragment extends BaseFollowFriendsTabFragment {
 
     private static final String TAG = FollowExpertsTabFragment.class.getSimpleName();
 
     private InfluencerAccountsAdapter mAdapter = new InfluencerAccountsAdapter(this);
 
-    private ArrayList<AccountMinimal> mAccounts;
-
-    /**
-     * these maps are used to retain references to Account objects expecting updates to their
-     * relationship status. This way, when the FollowAccountEvent returns, we don't have to iterate
-     * through our account list to find the account object to modify.
-     */
-    private HashMap<String, AccountMinimal> mAccountsExpectingUpdate
-            = new HashMap<String, AccountMinimal>();
-
-    private HashMap<String, Integer> mAccountExpectedRelationship
-            = new HashMap<String, Integer>();
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        App.injectMembers(this);
+    public InfluencerAccountsAdapter getAdapter() {
+        return mAdapter;
     }
 
     @Override
@@ -67,38 +37,4 @@ public class FollowExpertsTabFragment extends BaseFragment
         }
     }
 
-    public void onEventMainThread(FollowAccountEvent event) {
-
-        String accountId = event.getAccountId();
-        AccountMinimal account = mAccountsExpectingUpdate.remove(accountId);
-        int relationship = mAccountExpectedRelationship.remove(accountId);
-
-        if (event.isSuccessful()) {
-            account.setCurrentUserRelationship(relationship);
-        } else {
-            showToastError(event.getErrorMessage());
-        }
-        //will reset following toggle button back to original setting if error
-        mAdapter.notifyDataSetChanged();
-    }
-
-    public void onEventMainThread(FetchFriendSuggestionsEvent event) {
-        if (event.isSuccessful()) {
-            mAccounts = event.getAccounts();
-            mAdapter.setAccounts(event.getAccounts());
-            mAdapter.notifyDataSetChanged();
-            return;
-        }
-        //event error
-        showToastError(event.getErrorMessage());
-    }
-
-    @Override
-    public void toggleFollow(AccountMinimal account, boolean isFollowing) {
-        int relationship = isFollowing ? AccountMinimal.RELATION_TYPE_FOLLOWING
-                : AccountMinimal.RELATION_TYPE_NONE;
-        mAccountsExpectingUpdate.put(account.getId(), account);
-        mAccountExpectedRelationship.put(account.getId(), relationship);
-        mAccountController.followAccount(account.getId(), isFollowing);
-    }
 }
--- Delectable/app/src/main/java/com/delectable/mobile/ui/followfriends/fragment/FollowFacebookFriendsTabFragment.java
+++ Delectable/app/src/main/java/com/delectable/mobile/ui/followfriends/fragment/FollowFacebookFriendsTabFragment.java
@@ -5,6 +5,7 @@ import com.delectable.mobile.R;
 import com.delectable.mobile.api.models.AccountMinimal;
 import com.delectable.mobile.controllers.AccountController;
 import com.delectable.mobile.events.accounts.FetchFriendSuggestionsEvent;
+import com.delectable.mobile.events.accounts.FollowAccountEvent;
 import com.delectable.mobile.ui.BaseFragment;
 import com.delectable.mobile.ui.followfriends.widget.ContactsAdapter;
 import com.delectable.mobile.ui.followfriends.widget.FollowActionsHandler;
@@ -22,25 +23,19 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.inject.Inject;
 
-public class FollowFacebookFriendsTabFragment extends BaseFragment implements FollowActionsHandler {
+public class FollowFacebookFriendsTabFragment extends BaseFollowFriendsTabFragment {
 
     private static final String TAG = FollowFacebookFriendsTabFragment.class.getSimpleName();
 
-    @Inject
-    protected AccountController mAccountController;
-
     private ContactsAdapter mAdapter = new ContactsAdapter(this);
 
-    private ArrayList<AccountMinimal> mAccounts;
-
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        App.injectMembers(this);
+    public ContactsAdapter getAdapter() {
+        return mAdapter;
     }
 
     @Override
@@ -60,19 +55,4 @@ public class FollowFacebookFriendsTabFragment extends BaseFragment implements Fo
         }
     }
 
-    public void onEventMainThread(FetchFriendSuggestionsEvent event) {
-        if (event.isSuccessful()) {
-            mAccounts = event.getAccounts();
-            mAdapter.setAccounts(event.getAccounts());
-            mAdapter.notifyDataSetChanged();
-            return;
-        }
-        //event error
-        showToastError(event.getErrorMessage());
-    }
-
-    @Override
-    public void toggleFollow(AccountMinimal account, boolean isFollowing) {
-        Log.d(TAG, "toggled: " + account.getFullName() + " " + isFollowing);
-    }
 }
