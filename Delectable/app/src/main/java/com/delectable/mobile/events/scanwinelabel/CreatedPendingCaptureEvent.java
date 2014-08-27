4a1a6e505c641dce7e3b03e137c3f2120749eedc
Adam Bednarek <adam@ad60.com>
2014-08-26 22:59:53 -0400

    Create Pending Capture Job/Request/Response/Event

---
+++ Delectable/app/src/main/java/com/delectable/mobile/events/scanwinelabel/CreatedPendingCaptureEvent.java
@@ -0,0 +1,18 @@
+package com.delectable.mobile.events.scanwinelabel;
+
+import com.delectable.mobile.api.models.PendingCapture;
+import com.delectable.mobile.events.BaseEvent;
+
+public class CreatedPendingCaptureEvent extends BaseEvent {
+
+    private PendingCapture mPendingCapture;
+
+    public CreatedPendingCaptureEvent(PendingCapture pendingCapture) {
+        super(true);
+        mPendingCapture = pendingCapture;
+    }
+
+    public CreatedPendingCaptureEvent(String errorMessage) {
+        super(errorMessage);
+    }
+}
