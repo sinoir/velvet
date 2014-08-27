9fd0d913e782e78d7bf8509f5ed04fc35ba5d937
Adam Bednarek <adam@ad60.com>
2014-08-27 11:58:40 -0400

    Cleanup tests

--- Delectable/app/src/main/java/com/delectable/mobile/api/models/ProvisionCapture.java
+++ Delectable/app/src/main/java/com/delectable/mobile/api/models/ProvisionCapture.java
@@ -16,16 +16,32 @@ public class ProvisionCapture extends BaseResponse {
         return bucket;
     }
 
+    public void setBucket(String bucket) {
+        this.bucket = bucket;
+    }
+
     public String getFilename() {
         return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
     }
 
     public String getUrl() {
         return url;
     }
 
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
     public HashMap<String, String> getHeaders() {
         return headers;
+    }
+
+    public void setHeaders(HashMap<String, String> headers) {
+        this.headers = headers;
     }
 
     @Override
