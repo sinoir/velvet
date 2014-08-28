e356a70f8537a0c2ff95ac311fc66d31ea9bf89a
Adam Bednarek <adam@ad60.com>
2014-08-27 22:34:24 -0400

    Stub in ability to pass IDs for Wine Profile from Deep Link
    - Untested, still needs work.  For now integrate features to get IDs for WineProfile from Deep Links

--- Delectable/app/src/main/java/com/delectable/mobile/ui/wineprofile/activity/WineProfileActivity.java
+++ Delectable/app/src/main/java/com/delectable/mobile/ui/wineprofile/activity/WineProfileActivity.java
@@ -4,7 +4,6 @@ import com.delectable.mobile.R;
 import com.delectable.mobile.api.models.PhotoHash;
 import com.delectable.mobile.api.models.WineProfile;
 import com.delectable.mobile.ui.BaseActivity;
-import com.delectable.mobile.ui.profile.fragment.UserProfileFragment;
 import com.delectable.mobile.ui.wineprofile.fragment.WineProfileFragment;
 
 import android.os.Bundle;
@@ -13,10 +12,20 @@ import android.view.MenuItem;
 public class WineProfileActivity extends BaseActivity {
 
     public static final String PARAMS_WINE_PROFILE = "PARAMS_WINE_PROFILE";
+
     public static final String PARAMS_CAPTURE_PHOTO_HASH = "PARAMS_CAPTURE_PHOTO_HASH";
 
+    public static final String PARAMS_BASE_WINE_ID = "PARAMS_BASE_WINE_ID";
+
+    public static final String PARAMS_VINTAGE_ID = "PARAMS_VINTAGE_ID";
+
     private WineProfile mWineProfile;
+
     private PhotoHash mCapturePhotoHash;
+
+    private String mBaseWineId;
+
+    private String mVintageId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +35,24 @@ public class WineProfileActivity extends BaseActivity {
         if (args != null) {
             mWineProfile = args.getParcelable(PARAMS_WINE_PROFILE);
             mCapturePhotoHash = args.getParcelable(PARAMS_CAPTURE_PHOTO_HASH);
+            mBaseWineId = args.getString(PARAMS_BASE_WINE_ID);
+            mVintageId = args.getString(PARAMS_VINTAGE_ID);
+        } else {
+            // Check if Deep Link params contains data if the bundle args doesn't
+            mBaseWineId = getDeepLinkParam("base_wine_id");
+            mVintageId = getDeepLinkParam("vintage_id");
         }
+
         if (savedInstanceState == null) {
+            WineProfileFragment fragment = null;
+            if (mWineProfile != null && mCapturePhotoHash != null) {
+                fragment = WineProfileFragment.newInstance(mWineProfile, mCapturePhotoHash);
+            } else if (mBaseWineId != null) {
+                fragment = WineProfileFragment.newInstance(mBaseWineId, mVintageId);
+            }
             getFragmentManager().beginTransaction()
-                    .add(R.id.container, WineProfileFragment.newInstance(mWineProfile, mCapturePhotoHash)).commit();
+                    .add(R.id.container, fragment)
+                    .commit();
         }
 
         getActionBar().setDisplayHomeAsUpEnabled(true);
