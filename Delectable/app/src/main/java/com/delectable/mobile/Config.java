1bc17a148f7074c496ed0b8d26b3390a55cc23e9
Wayne Chiu <wayne@delectable.com>
2014-10-01 18:25:50 -0700

    API & Account object updates
    - made default session type in API calls android instead of mobile
    - updated Account.ClientState.activity_feed_ts_last from int to float

--- Delectable/app/src/main/java/com/delectable/mobile/Config.java
+++ Delectable/app/src/main/java/com/delectable/mobile/Config.java
@@ -2,7 +2,7 @@ package com.delectable.mobile;
 
 public class Config {
 
-    public static final String DEFAULT_SESSION_TYPE = "mobile";
+    public static final String DEFAULT_SESSION_TYPE = "android";
 
     public static final String FOURSQUARE_CLIENT_ID = "KPQWOHM01W4ZT32MQKMLJISLH0OIS5MDRQ44BVAYRTFL0UVG";
 
