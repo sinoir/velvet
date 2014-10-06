c1dd3c8719e53d06b619daf3a498d5c459b23f7a
Henning Post <henningpost@gmail.com>
2014-10-06 23:33:32 +0200

    BaseJob reruns on non-API errors

--- Delectable/app/src/main/java/com/delectable/mobile/jobs/BaseJob.java
+++ Delectable/app/src/main/java/com/delectable/mobile/jobs/BaseJob.java
@@ -6,8 +6,6 @@ import com.delectable.mobile.util.DelException;
 import com.path.android.jobqueue.Job;
 import com.path.android.jobqueue.Params;
 
-import android.util.Log;
-
 import javax.inject.Inject;
 
 import de.greenrobot.event.EventBus;
@@ -44,15 +42,13 @@ public class BaseJob extends Job {
 
     @Override
     protected boolean shouldReRunOnThrowable(Throwable throwable) {
-        // TODO check error type and see if a retry makes sense
         mErrorMessage = throwable.getMessage();
         if (throwable instanceof DelException) {
             int error = ((DelException) throwable).getErrorCode();
             mErrorCode = ErrorUtil.valueOfCode(error);
+            return false;
         }
-        Log.e(TAG + ".Error", "", throwable);
-
-        return false;
+        return true;
     }
 
     public String getErrorMessage() {
@@ -78,4 +74,5 @@ public class BaseJob extends Job {
     public NetworkClient getNetworkClient() {
         return mNetworkClient;
     }
+
 }
