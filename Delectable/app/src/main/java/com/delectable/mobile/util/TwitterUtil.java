66d4aa9b92761b6f77eef596e65e903abb9507c0
Wayne Chiu <wayne@delectable.com>
2014-10-07 13:35:32 -0700

    completed twitter integration for posting
    - unable to test, something seems to be wrong with twitter sdk

--- Delectable/app/src/main/java/com/delectable/mobile/util/TwitterUtil.java
+++ Delectable/app/src/main/java/com/delectable/mobile/util/TwitterUtil.java
@@ -4,31 +4,81 @@ import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.Twitter;
 import com.twitter.sdk.android.core.TwitterApiClient;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.models.Tweet;
 import com.twitter.sdk.android.core.services.StatusesService;
 
+import android.content.Context;
+import android.content.res.AssetManager;
 import android.util.Log;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Date;
+import java.util.Properties;
+
+import io.fabric.sdk.android.Fabric;
 
 public class TwitterUtil {
 
     private static final String TAG = TwitterUtil.class.getSimpleName();
 
+    private static final int API_KEY = 0;
+
+    private static final int API_SECRET = 1;
+
+    public static void init(Context context) {
+        String[] twitter = getTwitterApiKeyAndSecret(context);
+        TwitterAuthConfig authConfig =
+                new TwitterAuthConfig(
+                        twitter[API_KEY],
+                        twitter[API_SECRET]);
+        //Log.d(TAG, "twitter apikey: " + twitter[API_KEY]);
+        //Log.d(TAG, "twitter apisecret: " + twitter[API_SCRET]);
+        Fabric.with(context, new Twitter(authConfig));
+    }
+
+    private static String[] getTwitterApiKeyAndSecret(Context context) {
+        String apiKey = null;
+        String apiSecret = null;
+        try {
+            AssetManager assetManager = context.getAssets();
+            InputStream inputStream = assetManager.open("twitter_credentials.properties");
+
+            Properties properties = new Properties();
+            properties.load(inputStream);
+            apiKey = properties.getProperty("API_KEY");
+            apiSecret = properties.getProperty("API_SECRET");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String[] twitterCreds = new String[2];
+        twitterCreds[API_KEY] = apiKey;
+        twitterCreds[API_SECRET] = apiSecret;
+        return twitterCreds;
+    }
+
     /**
      * Checks to see that there is an active twitter session.
-     * @return
      */
     public static boolean isLoggedIn() {
         if (Twitter.getSessionManager().getActiveSession() == null) {
             return false;
         }
+        debug();
         return true;
     }
 
     public static void clearSession() {
         Twitter.getSessionManager().clearActiveSession();
+    }
+
+    public static void tweet(String message, Callback<Tweet> callback) {
+        TwitterApiClient twitterApiClient = Twitter.getApiClient();
+        StatusesService statusesService = twitterApiClient.getStatusesService();
+        statusesService.update(message, null, null, null, null, null, null, null, callback);
     }
 
     public static void debug() {
@@ -40,25 +90,21 @@ public class TwitterUtil {
         Log.d(TAG, "userName: " + userName);
         Log.d(TAG, "authToken: " + authToken);
 
-        //shoots out tweet
-        TwitterApiClient twitterApiClient = Twitter.getApiClient();
-        StatusesService statusesService = twitterApiClient.getStatusesService();
-        String tweet = "Hello from the delectable android app! " + new Date().toString();
-        statusesService
-                .update(tweet, null, null, null, null, null, null, null, new Callback<Tweet>() {
-                    @Override
-                    public void success(Result<Tweet> tweetResult) {
-                        Log.d(TAG, "tweet success!");
-                    }
-
-                    @Override
-                    public void failure(TwitterException e) {
-                        Log.d(TAG, "tweet fail");
-                        Log.d(TAG, "TwitterException", e);
-                        e.getMessage();
-                    }
-                });
+        tweet("Hello from the delectable android app! " + new Date().toString(), TwitterCallback);
     }
+
+    private static Callback<Tweet> TwitterCallback = new Callback<Tweet>() {
+        @Override
+        public void success(Result<Tweet> tweetResult) {
+            Log.d(TAG, "tweet success!");
+        }
+
+        @Override
+        public void failure(TwitterException e) {
+            Log.d(TAG, "tweet fail");
+            Log.d(TAG, "TwitterException", e);
+        }
+    };
 
 
 }
