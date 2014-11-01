c78dd806b9b6e772a6ef88a0a4d3cbf8b7cea770
Henning Post <henningpost@gmail.com>
2014-10-31 17:03:21 -0700

    capture details: fixed padding in list showing liking people

--- Delectable/app/src/main/java/com/delectable/mobile/ui/capture/widget/LikingPeopleAdapter.java
+++ Delectable/app/src/main/java/com/delectable/mobile/ui/capture/widget/LikingPeopleAdapter.java
@@ -1,8 +1,10 @@
 package com.delectable.mobile.ui.capture.widget;
 
+import com.delectable.mobile.R;
 import com.delectable.mobile.api.models.AccountMinimal;
 import com.delectable.mobile.ui.common.widget.PeopleRow;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -36,7 +38,8 @@ public class LikingPeopleAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         PeopleRow row = (PeopleRow) convertView;
         if (row == null) {
-            row = new PeopleRow(parent.getContext());
+            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+            row = (PeopleRow) inflater.inflate(R.layout.row_people_impl, parent, false);
         }
         AccountMinimal likingUser = getItem(position);
         row.updateData(likingUser.getPhoto().getBestThumb(), likingUser.getFullName());
