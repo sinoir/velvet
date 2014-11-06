528a70ac4f446b4531d0a6f89afef4104872bc1b
Adam Bednarek <adam@ad60.com>
2014-11-06 10:44:14 -0500

    Add VintageWineInfo ViewModel
    - View model to encapsulate the pricing / other info for Wine and Pricing
    - Implements Pricing info for now.

---
+++ Delectable/app/src/main/java/com/delectable/mobile/ui/wineprofile/viewmodel/VintageWineInfo.java
@@ -0,0 +1,55 @@
+package com.delectable.mobile.ui.wineprofile.viewmodel;
+
+import com.delectable.mobile.api.models.PurchaseOffer;
+import com.delectable.mobile.api.models.WineProfileSubProfile;
+
+/**
+ * View Model to help display Vintage / Pricing Info based on PurchaseOffer object or WineProfile
+ * object
+ */
+public class VintageWineInfo {
+
+    private PurchaseOffer mPurchaseOffer;
+
+    private WineProfileSubProfile mWineProfileMinimal;
+
+    public VintageWineInfo(WineProfileSubProfile wineProfileMinimal) {
+        this(wineProfileMinimal, null);
+    }
+
+    public VintageWineInfo(WineProfileSubProfile wineProfileMinimal, PurchaseOffer purchaseOffer) {
+        mPurchaseOffer = purchaseOffer;
+        mWineProfileMinimal = wineProfileMinimal;
+    }
+
+    public boolean isSoldOut() {
+        // Wine is Sold Out when the price is set to -1.
+        return mWineProfileMinimal != null && mWineProfileMinimal.getPrice() != null
+                && mWineProfileMinimal.getPrice() < 0;
+    }
+
+    public boolean hasPrice() {
+        return hasPurchaseOfferPrice() || hasWineProfilePrice();
+    }
+
+    public String getPriceText() {
+        if (hasPurchaseOfferPrice()) {
+            return mPurchaseOffer.getPricing().get(0).getPerBbottle();
+        }
+
+        if (hasWineProfilePrice()) {
+            return mWineProfileMinimal.getPriceText();
+        }
+
+        return "";
+    }
+
+    private boolean hasPurchaseOfferPrice() {
+        return mPurchaseOffer != null && mPurchaseOffer.getPricing() != null
+                && mPurchaseOffer.getPricing().size() > 0;
+    }
+
+    private boolean hasWineProfilePrice() {
+        return mWineProfileMinimal != null && mWineProfileMinimal.getPriceText() != null;
+    }
+}
