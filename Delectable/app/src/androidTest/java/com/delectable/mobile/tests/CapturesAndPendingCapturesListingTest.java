180b6b61f38786aa527997fcb248822b0c91ace0
Wayne Chiu <wayne@delectable.com>
2014-11-06 17:50:53 -0800

    tests for when pendingCapture converts to captureDetails

--- Delectable/app/src/androidTest/java/com/delectable/mobile/tests/CapturesAndPendingCapturesListingTest.java
+++ Delectable/app/src/androidTest/java/com/delectable/mobile/tests/CapturesAndPendingCapturesListingTest.java
@@ -32,76 +32,20 @@ public class CapturesAndPendingCapturesListingTest extends BaseInstrumentationTe
     }
 
     //for gson deserialization
-    private static final Type TYPE = new TypeToken<ListingResponse<BaseListingElement, DeleteHash>>() {
+    private static final Type TYPE
+            = new TypeToken<ListingResponse<BaseListingElement, DeleteHash>>() {
     }.getType();
 
     {
         GsonBuilder gsonBuilder = new GsonBuilder();
         //custom deserializer for captures_and_pending_captures endpoint because a mixed array returns
-        gsonBuilder.registerTypeAdapter(BaseListingElement.class, new BaseListingElementDeserializer());
+        gsonBuilder.registerTypeAdapter(BaseListingElement.class,
+                new BaseListingElementDeserializer());
         mGson = gsonBuilder.create();
     }
 
-    public void testParseCapturesAndPendingCapturesListing() throws JSONException {
-        JSONObject json = loadJsonObjectFromResource(
-                R.raw.test_accounts_captures_and_pending_captures_details_ctx_stage_1_raw_fetch);
-
-        ListingResponse<BaseListingElement, String> response = mGson.fromJson(json.toString(), TYPE);
-        Listing<BaseListingElement, String> actualListing = response.getPayload();
-
-        int pendingCapturesCount = 0;
-        int captureDetailsCount = 0;
-
-        for (BaseListingElement item : actualListing.getUpdates()) {
-            if (item instanceof PendingCapture) {
-                pendingCapturesCount++;
-            }
-
-            if (item instanceof CaptureDetails) {
-                captureDetailsCount++;
-            }
-        }
-
-        assertEquals(2, pendingCapturesCount);
-        assertEquals(8, captureDetailsCount);
-
-        assertEquals(10, actualListing.getUpdates().size());
-    }
-
-    public void testCapturesAndPendingCapturesListingWithAfterAndDelete() throws JSONException {
-        JSONObject json1 = loadJsonObjectFromResource(
-                R.raw.test_accounts_captures_and_pending_captures_details_ctx_stage_1_raw_fetch);
-        JSONObject json2WithAfter = loadJsonObjectFromResource(
-                R.raw.test_accounts_captures_and_pending_captures_details_ctx_stage_2_with_after);
-        JSONObject json3WithDelete = loadJsonObjectFromResource(
-                R.raw.test_accounts_captures_and_pending_captures_details_ctx_stage_3_with_delete);
-
-        ListingResponse<BaseListingElement, DeleteHash> response1, response2WithAfter, response3WithDelete;
-        response1 = mGson.fromJson(json1.toString(), TYPE);
-        response2WithAfter = mGson.fromJson(json2WithAfter.toString(), TYPE);
-        response3WithDelete = mGson.fromJson(json3WithDelete.toString(), TYPE);
-
-        Listing<BaseListingElement, DeleteHash> listing1, listing2WithAfter, listing3WithDelete;
-
-        listing1 = response1.getPayload();
-        listing2WithAfter = response2WithAfter.getPayload();
-        listing3WithDelete = response3WithDelete.getPayload();
-
-        ArrayList<BaseListingElement> displayData = listing1.getUpdates();
-
-        assertEquals(displayData, 2, 8, 10);
-
-        listing2WithAfter.combineInto(displayData, response2WithAfter.isInvalidate());
-
-        assertEquals(displayData, 3, 8, 11);
-
-        listing3WithDelete.combineInto(displayData, response3WithDelete.isInvalidate());
-
-        assertEquals(displayData, 2, 8, 10);
-
-    }
-
-    private static void assertEquals(ArrayList<BaseListingElement> displayData, int expectedPendingCount, int expectedCaptureCount,
+    private static void assertCountsEquals(ArrayList<BaseListingElement> displayData,
+            int expectedPendingCount, int expectedCaptureCount,
             int expectedTotal) {
         int actualPendingCapturesCount = 0;
         int actualCaptureDetailsCount = 0;
@@ -121,26 +65,101 @@ public class CapturesAndPendingCapturesListingTest extends BaseInstrumentationTe
         assertEquals(expectedTotal, displayData.size());
     }
 
-    //TODO test all combining with updates/deletes/before/after
-    //the captures_and_pending_captures endpoint does something different where items can show up
-    //updates as well as deletes when a pending capture is converted to a capture. we need to keep track of
-    //items we remove from the array and their original positions in the array, and then do a check against those removed values when we look
-    //through our updates array to see if it was in both arrays. If they were in both arrays then we need to insert that update back
-    //into it's original position in the list. the ids for pending capture and it's corresponding capture are
-    //guaranteed to be the same
-    public void testGetAllCombinedCaptures() throws JSONException {
+    public void testParseCapturesAndPendingCapturesListing() throws JSONException {
+        JSONObject json = loadJsonObjectFromResource(
+                R.raw.test_accounts_captures_and_pending_captures_details_ctx_stage_1_raw_fetch);
+
+        ListingResponse<BaseListingElement, String> response = mGson
+                .fromJson(json.toString(), TYPE);
+        Listing<BaseListingElement, String> actualListing = response.getPayload();
+
+        assertCountsEquals(actualListing.getUpdates(), 2, 8, 10);
+    }
+
+    public void testCapturesAndPendingCapturesListingWithAfterAndDelete() throws JSONException {
+        JSONObject json1 = loadJsonObjectFromResource(
+                R.raw.test_accounts_captures_and_pending_captures_details_ctx_stage_1_raw_fetch);
+        JSONObject json2WithAfter = loadJsonObjectFromResource(
+                R.raw.test_accounts_captures_and_pending_captures_details_ctx_stage_2_with_after);
+        JSONObject json3WithDelete = loadJsonObjectFromResource(
+                R.raw.test_accounts_captures_and_pending_captures_details_ctx_stage_3_with_delete);
+
+        ListingResponse<BaseListingElement, DeleteHash> response1, response2WithAfter,
+                response3WithDelete;
+        response1 = mGson.fromJson(json1.toString(), TYPE);
+        response2WithAfter = mGson.fromJson(json2WithAfter.toString(), TYPE);
+        response3WithDelete = mGson.fromJson(json3WithDelete.toString(), TYPE);
+
+        Listing<BaseListingElement, DeleteHash> listing1, listing2WithAfter, listing3WithDelete;
+
+        listing1 = response1.getPayload();
+        listing2WithAfter = response2WithAfter.getPayload();
+        listing3WithDelete = response3WithDelete.getPayload();
+
+        ArrayList<BaseListingElement> displayData = listing1.getUpdates();
+
+        assertCountsEquals(displayData, 2, 8, 10);
+
+        listing2WithAfter.combineInto(displayData, response2WithAfter.isInvalidate());
+
+        assertCountsEquals(displayData, 3, 8, 11);
+
+        listing3WithDelete.combineInto(displayData, response3WithDelete.isInvalidate());
+
+        assertCountsEquals(displayData, 2, 8, 10);
 
     }
 
-    public void testGetAllCombinedCapturesWithDeletions() throws JSONException {
+    /**
+     * Testing for when a pending capture becomes a capture. The same id will appear in the both the
+     * deletes array and the updates array.
+     */
+    public void testCapturesAndPendingCapturesListingWithConvertedCapture() throws JSONException {
+        JSONObject json1 = loadJsonObjectFromResource(
+                R.raw.test_pending_capture_to_capture_details_ctx_stage_1_raw_fetch);
+        JSONObject json2WithConvertedCapture = loadJsonObjectFromResource(
+                R.raw.test_pending_capture_to_capture_details_ctx_stage_2_capture_converted);
 
+        ListingResponse<BaseListingElement, DeleteHash> response1, response2WithConvertedCapture;
+        response1 = mGson.fromJson(json1.toString(), TYPE);
+        response2WithConvertedCapture = mGson.fromJson(json2WithConvertedCapture.toString(), TYPE);
+
+        Listing<BaseListingElement, DeleteHash> listing1, listing2WithConvertedCapture;
+        listing1 = response1.getPayload();
+        listing2WithConvertedCapture = response2WithConvertedCapture.getPayload();
+        String toDeleteId = listing2WithConvertedCapture.getDeletes().get(0).toString();
+
+        ArrayList<BaseListingElement> displayData = listing1.getUpdates();
+
+        assertCountsEquals(displayData, 4, 6, 10);
+
+        int convertingCapturePosition = -1;
+        for (int i = 0; i < displayData.size(); i++) {
+            BaseListingElement item = displayData.get(i);
+            if (item.getId().equals(toDeleteId)) {
+                convertingCapturePosition = i;
+                break;
+            }
+        }
+        BaseListingElement pendingCapture = displayData.get(convertingCapturePosition);
+        assertTrue(pendingCapture instanceof PendingCapture);
+
+
+        listing2WithConvertedCapture
+                .combineInto(displayData, response2WithConvertedCapture.isInvalidate());
+
+        assertCountsEquals(displayData, 3, 7, 10);
+
+        BaseListingElement convertedCapture = displayData.get(convertingCapturePosition);
+        assertEquals(convertedCapture.getId(), toDeleteId);
+        assertTrue(convertedCapture instanceof CaptureDetails);
     }
-
 
     public void testGetFromScratchListing() throws JSONException {
         JSONObject json = loadJsonObjectFromResource(
                 R.raw.test_accounts_captures_and_pending_captures_details_ctx);
-        ListingResponse<BaseListingElement, String> response = mGson.fromJson(json.toString(), TYPE);
+        ListingResponse<BaseListingElement, String> response = mGson
+                .fromJson(json.toString(), TYPE);
         Listing<BaseListingElement, String> listing = response.getPayload();
 
         ArrayList<BaseListingElement> expectedCaptures = listing.getUpdates();
@@ -152,7 +171,4 @@ public class CapturesAndPendingCapturesListingTest extends BaseInstrumentationTe
         assertEquals(expectedCaptures, actualCaptures);
     }
 
-    public void testCombinedCapturesOrder() throws JSONException {
-        //TODO write test
-    }
 }
