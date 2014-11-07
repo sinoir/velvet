e718fc5b858c74d5a4f6d43aceceea0475128899
Adam Bednarek <adam@ad60.com>
2014-11-07 15:37:54 -0500

    Fetch WineSource in CheckoutFragment

--- Delectable/app/src/main/java/com/delectable/mobile/ui/winepurchase/fragment/WineCheckoutFragment.java
+++ Delectable/app/src/main/java/com/delectable/mobile/ui/winepurchase/fragment/WineCheckoutFragment.java
@@ -2,6 +2,11 @@ package com.delectable.mobile.ui.winepurchase.fragment;
 
 import com.delectable.mobile.App;
 import com.delectable.mobile.R;
+import com.delectable.mobile.api.cache.WineSourceModel;
+import com.delectable.mobile.api.controllers.BaseWineController;
+import com.delectable.mobile.api.events.wines.FetchedWineSourceEvent;
+import com.delectable.mobile.api.models.PurchaseOffer;
+import com.delectable.mobile.api.models.WineProfileMinimal;
 import com.delectable.mobile.ui.BaseFragment;
 
 import android.os.Bundle;
@@ -9,13 +14,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import javax.inject.Inject;
+
 import butterknife.ButterKnife;
 
 public class WineCheckoutFragment extends BaseFragment {
 
     private static final String ARGS_VINTAGE_ID = "ARGS_VINTAGE_ID";
 
+    @Inject
+    protected BaseWineController mBaseWineController;
+
+    @Inject
+    protected WineSourceModel mWineSourceModel;
+
     private String mVintageId;
+
+    private WineProfileMinimal mWineProfile;
+
+    private PurchaseOffer mPurchaseOffer;
 
     public static WineCheckoutFragment newInstance(String vintageId) {
         WineCheckoutFragment fragment = new WineCheckoutFragment();
@@ -41,6 +58,37 @@ public class WineCheckoutFragment extends BaseFragment {
             Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_wine_checkout, null, false);
         ButterKnife.inject(this, view);
+
+        // Always Fetch Latest Wine Source
+        fetchWineSource();
+        
         return view;
     }
+
+    //region Load Local Data
+    private void loadPricingData() {
+        mWineProfile = mWineSourceModel.getMinWineWithPrice(mVintageId);
+        mPurchaseOffer = mWineSourceModel.getPurchaseOffer(mVintageId);
+    }
+    //endregion
+
+    //region Fetch Remote Data
+    private void fetchWineSource() {
+        mBaseWineController.fetchWineSource(mVintageId);
+    }
+    //endregion
+
+    //region EventBus
+    public void onEventMainThread(FetchedWineSourceEvent event) {
+        if (!mVintageId.equalsIgnoreCase(event.getWineId())) {
+            return;
+        }
+
+        if (!event.isSuccessful()) {
+            showToastError(event.getErrorMessage());
+        }
+
+        loadPricingData();
+    }
+    //endregion
 }
