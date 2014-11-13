aa19fb57c5a8fec778e2fd39ed9c1a605b6ce03c
Wayne Chiu <wayne@delectable.com>
2014-11-13 13:57:03 -0800

    added actionbar back navigation into webviewactivity

--- Delectable/app/src/main/java/com/delectable/mobile/ui/common/activity/WebViewActivty.java
+++ Delectable/app/src/main/java/com/delectable/mobile/ui/common/activity/WebViewActivty.java
@@ -7,6 +7,7 @@ import com.delectable.mobile.ui.common.fragment.WebViewFragment;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class WebViewActivty extends BaseActivity {
 
@@ -35,7 +36,7 @@ public class WebViewActivty extends BaseActivity {
         String title = args.getString(TITLE);
         setTitle(title);
 
-        setContentView(R.layout.activity_fragment_container);
+        setContentView(R.layout.activity_toolbar_fragment_container);
 
         String url = args.getString(URL);
 
@@ -48,5 +49,15 @@ public class WebViewActivty extends BaseActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finishDeepLinkActivity();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
