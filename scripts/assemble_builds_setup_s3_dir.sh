6350b8d7c6728597774044bd0c9f3397f000c56d
Wayne <wayne@delectable.com>
2014-11-24 12:49:42 -0500

    removed release build for superQa

--- scripts/assemble_builds_setup_s3_dir.sh
+++ scripts/assemble_builds_setup_s3_dir.sh
@@ -74,14 +74,12 @@ export GRADLE_OPTS=-Xmx2048m
 if [[ ${GIT_BRANCH_NO_PATH} == release ]];
 then
 	echo "assembling builds from release branch"
-	./gradlew assembleStagingPrerelease
-	./gradlew assembleStagingRelease
-	./gradlew assemblePickablePrerelease
-	./gradlew assemblePickableRelease
-	prepareForS3 staging-prerelease
-	prepareForS3 staging-release
-	prepareForS3 pickable-prerelease
-	prepareForS3 pickable-release
+	./gradlew assembleQaPrerelease
+	./gradlew assembleQaRelease
+	./gradlew assembleSuperQaRelease
+	prepareForS3 qa-prerelease
+	prepareForS3 qa-release
+	prepareForS3 superQa-release
 fi
 
 if [[ ${GIT_BRANCH_NO_PATH} == master ]];
