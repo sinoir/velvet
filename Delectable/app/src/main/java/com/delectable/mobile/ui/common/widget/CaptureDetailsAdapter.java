b76e2413fe4bcf33e8ab6e4b3fb39dec66a5fa64
Henning Post <henningpost@gmail.com>
2014-12-11 18:05:57 -0800

    cleanup: removed some unused leftovers

--- Delectable/app/src/main/java/com/delectable/mobile/ui/common/widget/CaptureDetailsAdapter.java
+++ Delectable/app/src/main/java/com/delectable/mobile/ui/common/widget/CaptureDetailsAdapter.java
@@ -3,7 +3,6 @@ package com.delectable.mobile.ui.common.widget;
 import com.delectable.mobile.R;
 import com.delectable.mobile.api.models.CaptureDetails;
 import com.delectable.mobile.ui.capture.widget.CaptureDetailsView;
-import com.delectable.mobile.ui.profile.widget.CaptureSimpleItemRow;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,14 +11,14 @@ import android.view.ViewGroup;
 public class CaptureDetailsAdapter extends InfiniteScrollAdapter<CaptureDetails> {
 
     public enum RowType {
-        SIMPLE, DETAIL, PURCHASE;
+        DETAIL, PURCHASE;
     }
 
     private CaptureDetailsView.CaptureActionsHandler mCaptureActionsHandler;
 
     private String mAccountId;
 
-    private RowType mRowType = RowType.SIMPLE;
+    private RowType mRowType = RowType.DETAIL;
 
     public CaptureDetailsAdapter(CaptureDetailsView.CaptureActionsHandler captureActionsHandler,
             ActionsHandler actionsHandler, String accountId) {
@@ -33,10 +32,6 @@ public class CaptureDetailsAdapter extends InfiniteScrollAdapter<CaptureDetails>
         super.getView(position, convertView, parent);
         View row;
         switch (mRowType) {
-            // TODO remove unused SIMPLE row type
-            case SIMPLE:
-                row = getSimpleCaptureRow(position, convertView, parent);
-                break;
             case PURCHASE:
                 row = getPurchaseCaptureRow(position, convertView, parent);
                 break;
@@ -46,21 +41,6 @@ public class CaptureDetailsAdapter extends InfiniteScrollAdapter<CaptureDetails>
                 break;
         }
         return row;
-    }
-
-    public View getSimpleCaptureRow(int position, View convertView, ViewGroup parent) {
-
-        CaptureSimpleItemRow rowView = (CaptureSimpleItemRow) convertView;
-
-        if (rowView == null) {
-            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-            rowView = (CaptureSimpleItemRow) inflater.inflate(R.layout.row_simple_wine_detail_impl,
-                    parent, false);
-            rowView.setActionsHandler(mCaptureActionsHandler);
-        }
-        CaptureDetails capture = mItems.get(position);
-        rowView.updateData(capture, mAccountId);
-        return rowView;
     }
 
     public View getPurchaseCaptureRow(int position, View convertView, ViewGroup parent) {
