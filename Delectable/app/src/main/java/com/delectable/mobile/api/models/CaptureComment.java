6ba107d1d09a956ee9cfc2e2d9b76c67348d3faf
Henning Post <henningpost@gmail.com>
2015-01-05 11:17:44 -0800

    added comment attributes to CaptureDetails

--- Delectable/app/src/main/java/com/delectable/mobile/api/models/CaptureComment.java
+++ Delectable/app/src/main/java/com/delectable/mobile/api/models/CaptureComment.java
@@ -62,4 +62,27 @@ public class CaptureComment {
                 ", comment_attributes=" + comment_attributes +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        CaptureComment that = (CaptureComment) o;
+
+        if (!id.equals(that.id)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
 }
