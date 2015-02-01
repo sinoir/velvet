8def8006d400c15b212bd4ac62d1e3ecc6e9fd74
Jesse Lee <mienaikoe@gmail.com>
2015-02-01 03:29:02 -0500

    zooming works right

--- app/src/main/java/com/mienaikoe/wifimesh/map/TrainView.java
+++ app/src/main/java/com/mienaikoe/wifimesh/map/TrainView.java
@@ -6,16 +6,20 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.support.v4.view.MotionEventCompat;
 import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
+import android.view.WindowManager;
 
 import com.mienaikoe.wifimesh.R;
 import com.mienaikoe.wifimesh.train.TrainLine;
@@ -43,6 +47,8 @@ public class TrainView extends View {
     private float startY = 0;
     private float deltaX = 200;
     private float deltaY = -50;
+    private float maxX;
+    private float maxY;
 
 
     private List<VectorInstruction> lines = new ArrayList<VectorInstruction>(0);
@@ -55,19 +61,29 @@ public class TrainView extends View {
 
     public TrainView(Context context) {
         super(context);
-        mScaleDetector = new ScaleGestureDetector(context, new ScaleListener());
+        init(context);
     }
 
     public TrainView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        mScaleDetector = new ScaleGestureDetector(context, new ScaleListener());
+        init(context);
     }
 
     public TrainView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        mScaleDetector = new ScaleGestureDetector(context, new ScaleListener());
+        init(context);
     }
 
+    private void init(Context context){
+        mScaleDetector = new ScaleGestureDetector(context, new ScaleListener());
+
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        this.maxX = size.x;
+        this.maxY = size.y;
+    }
 
 
 
@@ -77,9 +93,13 @@ public class TrainView extends View {
 
         this.setBackgroundColor(this.getResources().getColor(R.color.light_gray));
 
-        canvas.save();
-        canvas.scale(mScaleFactor, mScaleFactor, scalePointX, scalePointY);
         canvas.translate(deltaX / mScaleFactor, deltaY / mScaleFactor);
+
+        Log.i("SCALE FACTOR", mScaleFactor+"");
+
+        canvas.scale(
+                mScaleFactor, mScaleFactor,
+                scalePointX - deltaX, scalePointY - deltaY);
 
         for( VectorInstruction crossStreet : this.crossStreets ){
             crossStreet.draw(canvas);
@@ -103,7 +123,6 @@ public class TrainView extends View {
             }
         }
 
-        canvas.restore();
     }
 
 
@@ -114,13 +133,12 @@ public class TrainView extends View {
         public boolean onScale(ScaleGestureDetector detector) {
             mScaleFactor *= detector.getScaleFactor();
 
+            // Don't let the object get too small or too large.
+            mScaleFactor = Math.max(0.5f, Math.min(mScaleFactor, 5.0f));
+
             scalePointX = detector.getFocusX();
             scalePointY = detector.getFocusY();
 
-            // Don't let the object get too small or too large.
-            mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 5.0f));
-
-            invalidate();
             return true;
         }
     }
@@ -128,33 +146,57 @@ public class TrainView extends View {
 
 
 
+
+    private int mActivePointerId = -1;
+
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
-        // Let the ScaleGestureDetector inspect all events.
+
         mScaleDetector.onTouchEvent(ev);
 
-
-        final int action = MotionEventCompat.getActionMasked(ev);
-        final int pointerIndex = MotionEventCompat.getActionIndex(ev);
-        if( pointerIndex != 0 ){
-            return true;
-        }
-
-        switch (action) {
+        switch (MotionEventCompat.getActionMasked(ev)) {
             case MotionEvent.ACTION_DOWN: {
+                final int pointerIndex = MotionEventCompat.getActionIndex(ev);
                 startX = MotionEventCompat.getX(ev, pointerIndex) - deltaX;
                 startY = MotionEventCompat.getY(ev, pointerIndex) - deltaY;
+                mActivePointerId = MotionEventCompat.getPointerId(ev, 0);
                 break;
             }
             case MotionEvent.ACTION_MOVE: {
-                float atX = MotionEventCompat.getX(ev, pointerIndex);
-                float atY = MotionEventCompat.getY(ev, pointerIndex);
-                deltaX = atX - startX;
-                deltaY = atY - startY;
-                invalidate();
+                if( MotionEventCompat.getPointerCount(ev) <= 1 ) {
+                    final int pointerIndex = MotionEventCompat.findPointerIndex(ev, mActivePointerId);
+                    if (pointerIndex == -1) {
+                        mActivePointerId = -1;
+                        break;
+                    }
+                    float atX = MotionEventCompat.getX(ev, pointerIndex);
+                    float atY = MotionEventCompat.getY(ev, pointerIndex);
+                    deltaX = atX - startX;
+                    deltaY = atY - startY;
+                    break;
+                }
+            }
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL: {
+                mActivePointerId = -1;
+                break;
+            }
+            case MotionEvent.ACTION_POINTER_UP: {
+                final int pointerIndex = MotionEventCompat.getActionIndex(ev);
+                final int pointerId = MotionEventCompat.getPointerId(ev, pointerIndex);
+                if (pointerId == mActivePointerId) {
+                    // This was our active pointer going up. Choose a new
+                    // active pointer and adjust accordingly.
+                    final int newPointerIndex = pointerIndex == 0 ? 1 : 0;
+                    startX = MotionEventCompat.getX(ev, newPointerIndex) - deltaX;
+                    startY = MotionEventCompat.getY(ev, newPointerIndex) - deltaY;
+                    mActivePointerId = MotionEventCompat.getPointerId(ev, newPointerIndex);
+                }
                 break;
             }
         }
+
+        this.invalidate();
         return true;
     }
 
@@ -163,7 +205,7 @@ public class TrainView extends View {
         mScaleFactor = 2.5f;
         this.deltaX = -(x * mScaleFactor) + (getResources().getDisplayMetrics().widthPixels/2);
         this.deltaY = -(y * mScaleFactor) + (getResources().getDisplayMetrics().heightPixels/2);
-        this.invalidate();
+        invalidate();
     }
 
     public void setSystem(TrainSystem system) {
